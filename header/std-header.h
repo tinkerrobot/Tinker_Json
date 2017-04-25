663b118f5a1698613de993366712aa6d7730abcd
Ling.Li <liling44@outlook.com>
2017-04-25 10:55:32 +0800

    Ling.Li: 1. Initialization.

---
+++ header/std-header.h
@@ -0,0 +1,18 @@
+/*
+ * Project: Noob_Json_Parser
+ * File: std-header.h
+ * Author: Ling.Li
+ * Date: 2017/4/25
+ */
+
+#ifndef NOOB_JSON_PARSER_STD_HEADER_H
+#define NOOB_JSON_PARSER_STD_HEADER_H
+
+#include <cassert>
+#include <cerrno>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#endif //NOOB_JSON_PARSER_STD_HEADER_H
---
+++ source/test-function.cpp
@@ -0,0 +1,9 @@
+/*
+ * Project: Noob_Json_Parser
+ * File: test-function.cpp
+ * Author: Ling.Li
+ * Date: 2017/4/25
+ */
+
+#include "../header/std-header.h"
+
